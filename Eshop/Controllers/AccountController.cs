998659dbdf54ffedf8510d8b27a4d710934dd8da
Mahmood Ghanbari <contact.ghanbary@gmail.com>
2020-06-30 07:38:58 -0700

    intial commit

--- Eshop/Controllers/AccountController.cs
+++ Eshop/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Eshop.Controllers
 {
@@ -22,7 +23,26 @@ namespace Eshop.Controllers
         {
             if (ModelState.IsValid)
             {
+                if (!db.Users.Any(u => u.Email == register.Email.Trim().ToLower()))
+                {
+                    Users user = new Users()
+                    {
+                        Email = register.Email.Trim().ToLower(),
+                        password = FormsAuthentication.HashPasswordForStoringInConfigFile(register.Password, "MD5"),
+                        ActiveCode = Guid.NewGuid().ToString(),
+                        IsActive = false,
+                        RegisterDate = DateTime.Now,
+                        RoleID = 1,
+                        UserName = register.UserName
+                    };
 
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("Email", "کاربر با این ایمیل قبلا ثبت نام کرده است");
             }
             return View(register);
         }
